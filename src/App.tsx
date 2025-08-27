86ba88fb2053f105eb010df0aec20e17206affad
Laysa <laysa.viana@outlook.com>
2025-08-27 09:37:15 -0300

    adicionado Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from 'styled-components';
-import { AppContainer } from './App.styles';
+import { AppContainer, Main } from './App.styles';
 
 import { useState } from 'react';
 import { themes } from './theme';
@@ -7,23 +7,33 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import Home from './pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import { Reset } from 'styled-reset';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MovieDetails from './pages/MovieDetails/MovieDetails';
 
 const queryClient = new QueryClient();
 
 export default function App() {
-  const [darkMode, setDarkMode] = useState(true); 
+  const [darkMode, setDarkMode] = useState(true);
   const theme = darkMode ? themes.dark : themes.light;
 
   return (
     <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
-      <AppContainer>
-        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-        <main>
-          <Home />
-        </main>
-      </AppContainer>
-    </ThemeProvider>
+      <Reset />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <AppContainer>
+            <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+            <Main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+
+                <Route path="/movie/:id" element={<MovieDetails />} />
+              </Routes>
+            </Main>
+          </AppContainer>
+        </BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
