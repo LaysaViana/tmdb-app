eaaaa7768f66c073dcbd54ebff00d4b2a51cf64a
Laysa <laysa.viana@outlook.com>
2025-08-27 09:46:24 -0300

    styles do App

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
+import backgropund from '../src/assets/icons/backgropund-krists-luhaers-unsplash.png';
 
 export const AppContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.background};
   color: ${({ theme }) => theme.colors.textPrimary};
   min-height: 100vh;
-  max-width: 1280px;
   margin: 0 auto;
-  padding: 2rem;
   text-align: center;
+
+  background-image: url(${backgropund});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-origin: content-box;
+`;
+
+export const Main = styled.main`
+  padding: 0rem 2rem 2rem 2rem;
 `;
