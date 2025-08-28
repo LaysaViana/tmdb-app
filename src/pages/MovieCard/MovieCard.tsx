e9d6359d68b9e8cb2e129636ea8fc06c20ac6007
Laysa <laysa.viana@outlook.com>
2025-08-28 02:58:12 -0300

    add component recharts e ajustes styles

--- src/pages/MovieCard/MovieCard.tsx
+++ src/pages/MovieCard/MovieCard.tsx
@@ -1,11 +1,14 @@
-import { Card, Info, Poster } from './Movie.styles';
+import RechartsComponents from '../../components/Recharts/RechartsComponent';
+import { Card, Genres, Info, Overlay, Poster, Title } from './MovieCard.styles';
 import { useNavigate } from 'react-router-dom';
 
 interface MovieCardProps {
   id: number;
-  title: string;
-  posterUrl: string;
-  releaseDate: string;
+  title?: string;
+  posterUrl?: string;
+  releaseDate?: string;
+  genres?: { id: number; name: string }[];
+  vote_average?: number;
 }
 
 export default function MovieCard({
@@ -13,6 +16,8 @@ export default function MovieCard({
   title,
   posterUrl,
   releaseDate,
+  genres = [],
+  vote_average = 0,
 }: MovieCardProps) {
   const navigate = useNavigate();
 
@@ -23,9 +28,22 @@ export default function MovieCard({
   return (
     <Card onClick={handleClickDetails}>
       <Poster src={posterUrl} alt={title} />
+      <Overlay>
+        <Title>{title}</Title>
+        <Genres>
+          {genres.map((g, idx) => (
+            <span key={g.id ?? idx}>
+              {g.name}
+              {idx < genres.length - 1 ? ', ' : ''}
+            </span>
+          ))}
+        </Genres>
+
+        <RechartsComponents voteAverage={vote_average} />
+      </Overlay>
       <Info>
-        <h3>{title}</h3>
-        <p>{releaseDate}</p>
+        <h3>{title ?? 'Sem título'}</h3>
+        <p>{releaseDate ?? 'Data indisponível'}</p>
       </Info>
     </Card>
   );
